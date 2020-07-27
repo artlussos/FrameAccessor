8746746b796ab861fa31deceec87f942aa539b85
Art Lussos <art.lussos@gmail.com>
2020-07-27 13:17:10 -0400

    @origin, @size -> @viewOrigin, @viewSize

--- Examples/FrameAccessor/ViewController.m
+++ Examples/FrameAccessor/ViewController.m
@@ -15,42 +15,42 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
     // Center
     UIView *rectAtCenter = [UIView new];
     rectAtCenter.backgroundColor = [UIColor redColor];
-    rectAtCenter.size = CGSizeMake(100., 100.);
+    rectAtCenter.viewSize = CGSizeMake(100., 100.);
     rectAtCenter.center = self.view.middlePoint;
     [self.view addSubview:rectAtCenter];
-    
+
     // Top Left
     UIView *rectAtTopLeft = [UIView new];
     rectAtTopLeft.backgroundColor = [UIColor blueColor];
-    rectAtTopLeft.size = CGSizeMake(50., 50.);
+    rectAtTopLeft.viewSize = CGSizeMake(50., 50.);
     rectAtTopLeft.right = rectAtCenter.left - 25.;
     rectAtTopLeft.bottom = rectAtCenter.top - 25.;
     [self.view addSubview:rectAtTopLeft];
-    
+
     // Top Right
     UIView *rectAtTopRight = [UIView new];
     rectAtTopRight.backgroundColor = [UIColor magentaColor];
-    rectAtTopRight.size = CGSizeMake(50., 50.);
+    rectAtTopRight.viewSize = CGSizeMake(50., 50.);
     rectAtTopRight.left = rectAtCenter.right + 25.;
     rectAtTopRight.bottom = rectAtCenter.top - 25.;
     [self.view addSubview:rectAtTopRight];
-    
+
     // Bottom Left
     UIView *rectAtBottomLeft = [UIView new];
     rectAtBottomLeft.backgroundColor = [UIColor greenColor];
-    rectAtBottomLeft.size = CGSizeMake(50., 50.);
+    rectAtBottomLeft.viewSize = CGSizeMake(50., 50.);
     rectAtBottomLeft.right = rectAtTopLeft.right;
     rectAtBottomLeft.top = rectAtCenter.bottom + 25.;
     [self.view addSubview:rectAtBottomLeft];
-    
+
     // Bottom Right
     UIView *rectAtBottomRight = [UIView new];
     rectAtBottomRight.backgroundColor = [UIColor brownColor];
-    rectAtBottomRight.size = CGSizeMake(50., 50.);
+    rectAtBottomRight.viewSize = CGSizeMake(50., 50.);
     rectAtBottomRight.left = rectAtTopRight.left;
     rectAtBottomRight.top = rectAtCenter.bottom + 25.;
     [self.view addSubview:rectAtBottomRight];
